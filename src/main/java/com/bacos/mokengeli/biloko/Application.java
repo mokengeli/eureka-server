a13733b7d9e1f3e386a8de3ee68a967a9bdd7e9a
emmanuel <emmanuel.babala@dame-prevoyance.com>
2025-05-10 14:09:18 +0200

    feat: init project

--- src/main/java/com/bacos/mokengeli/biloko/Application.java
+++ src/main/java/com/bacos/mokengeli/biloko/Application.java
@@ -5,11 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+01:00"));
         SpringApplication.run(Application.class, args);
     }
 }
